d4e8c9b8f8552ea37cb66c462fcd03351055f07b
Sungzors <s1chun3636@gmail.com>
2016-09-26 12:07:59 -0700

    Creates searchresult base

---
+++ ViceNews/app/src/main/java/com/example/sungwon/vicenews/SearchResult.java
@@ -0,0 +1,23 @@
+package com.example.sungwon.vicenews;
+
+/**
+ * Created by SungWon on 9/26/2016.
+ */
+
+public class SearchResult {
+    NewsItem data;
+
+    public NewsItem getData() {
+        return data;
+    }
+
+    public void setData(NewsItem data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "SearchResult{" + data.getItems +
+                '}';
+    }
+}
