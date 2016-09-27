063dce5e7bd78802c2e2e6ea3e1fde4e67155202
Chris Rohr <chrisdrohr@gmail.com>
2016-09-27 09:46:14 -0700

    adds staggered grid layout

--- ViceNews/app/src/main/java/com/example/sungwon/vicenews/MainActivity.java
+++ ViceNews/app/src/main/java/com/example/sungwon/vicenews/MainActivity.java
@@ -14,8 +14,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
+    private StaggeredGridLayoutManager layoutManager;
 
     public static final int NOTIFICATION = 1;
 
@@ -53,11 +53,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         //TODO RECYCLER VIEW SETTINGS
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 //        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-//        recyclerView.setLayoutManager(layoutManager);
+        layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
 //        adapter = new RecyclerViewAdapter(dataSet);
 //        recyclerView.setAdapter(adapter);
 
