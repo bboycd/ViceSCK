4e42f3a9503603246c857c6b9d905bce8f59968f
Chris Rohr <chrisdrohr@gmail.com>
2016-09-27 12:07:45 -0700

    adds card view dimensions and image test

--- ViceNews/app/src/main/java/com/example/sungwon/vicenews/RecyclerViewAdapter.java
+++ ViceNews/app/src/main/java/com/example/sungwon/vicenews/RecyclerViewAdapter.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RecyclerViewAdapter extends CursorRecyclerViewAdapter<RecyclerView.ViewHolder> {
@@ -18,16 +19,23 @@ public class RecyclerViewAdapter extends CursorRecyclerViewAdapter<RecyclerView.
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView textView;
+        public ImageView imageView;
         public CardView cardView;
 
 
-        public ViewHolder(View itemView) {
-            super(itemView);
+        public ViewHolder(View view) {
+            super(view);
+
+            textView = (TextView) view.findViewById(R.id.textView);
+            imageView = (ImageView) view.findViewById(R.id.imageView);
+            cardView = (CardView) view.findViewById(R.id.cardView);
         }
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, Cursor cursor) {
+
+
 
     }
 
