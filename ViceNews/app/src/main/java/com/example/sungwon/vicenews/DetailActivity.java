dcfce83a21dced0315fa7c631c6887f389c95a9b
Sungzors <s1chun3636@gmail.com>
2016-09-29 15:36:52 -0700

    attempt detail layout

--- ViceNews/app/src/main/java/com/example/sungwon/vicenews/DetailActivity.java
+++ ViceNews/app/src/main/java/com/example/sungwon/vicenews/DetailActivity.java
@@ -2,15 +2,18 @@ package com.example.sungwon.vicenews;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import static com.android.volley.Request.Method.HEAD;
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
+
+    TextView mTitleText;
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,29 +22,37 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mTitleText = (TextView)findViewById(R.id.titleText);
+        mImageView = (ImageView)findViewById(R.id.imageView_Detail);
+
+
         //TODO TRANSISTION ANIMATION
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         Intent intent = new Intent();
 
         //WEBVIEW
         WebView webview = new WebView(this);
+        String title = intent.getStringExtra("title");
         String html = intent.getStringExtra("body");
+        String url = intent.getStringExtra("image");
         setContentView(webview);
         webview.loadData(html, "text/html", null);
+        Picasso.with(DetailActivity.this).load(url).fit();
+        mTitleText.setText(title);
 
-        FloatingActionButton fab_share = (FloatingActionButton) findViewById(R.id.fab_share);
-        fab_share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-
-        FloatingActionButton fab_readLater = (FloatingActionButton) findViewById(R.id.fab_readLater);
-        fab_readLater.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+//        FloatingActionButton fab_share = (FloatingActionButton) findViewById(R.id.fab_share);
+//        fab_share.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            }
+//        });
+//
+//        FloatingActionButton fab_readLater = (FloatingActionButton) findViewById(R.id.fab_readLater);
+//        fab_readLater.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
     }
 }
