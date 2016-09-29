c58f9f76e104e597e430362d7d35dda6eaa6c4c2
Chris Rohr <chrisdrohr@gmail.com>
2016-09-29 10:32:13 -0700

    nothing

--- ViceNews/app/src/main/java/com/example/sungwon/vicenews/MainActivity.java
+++ ViceNews/app/src/main/java/com/example/sungwon/vicenews/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
 
         /* Instantiating for SyncAdapter*/
         mAccount = createSyncAccount(this);
